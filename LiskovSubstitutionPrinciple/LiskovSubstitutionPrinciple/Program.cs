5d8bad0980844f9ce5b2ccb76704af11f0e0fd28
Harish Kumar A V <110531457+harishkumarAV@users.noreply.github.com>
2025-02-12 17:51:29 +0530

    Solid principles

---
+++ LiskovSubstitutionPrinciple/LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,39 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+class Bird
+{
+    public virtual void fly()
+    {
+        Console.WriteLine("Birds can Fly");
+    }
+}
+class Penguin : Bird
+{
+    public override void fly()
+    {
+        Console.WriteLine("Penguins can swim");
+    }
+}
+class Ostrich : Bird
+{
+    public override void fly()
+    {
+        Console.WriteLine("Ostriches can't fly");
+    }
+}
+class program
+{
+    public static void Main(string[] args)
+    {
+        Bird p = new Ostrich();
+        p.fly();
+
+        p = new Penguin();
+        p.fly();
+
+        p= new Bird(); 
+        p.fly();
+    }
+}
