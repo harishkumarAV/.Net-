0fc8f1d62ba5f68ac4b08d167e02b4d8f90dc176
Harish Kumar A V <110531457+harishkumarAV@users.noreply.github.com>
2025-02-12 17:44:24 +0530

    Async Programming Example

---
+++ AsyncProgramming/AsyncProgramming/Program.cs
@@ -0,0 +1,32 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncProgrammingDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Fetching data...");
+            string data = await FetchDataAsync("https://jsonplaceholder.typicode.com/posts");
+            Console.WriteLine("Data fetched:");
+            Console.WriteLine(data);
+        }
+
+        static async Task<string> FetchDataAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseData = await response.Content.ReadAsStringAsync();
+                return responseData;
+            }
+        }
+    }
+}
