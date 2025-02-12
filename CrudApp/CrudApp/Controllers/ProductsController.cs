1575730ba1a639f3804062c6bc475e706e139d1e
Harish Kumar A V <110531457+harishkumarAV@users.noreply.github.com>
2025-02-12 17:38:38 +0530

    Add files via upload

---
+++ CrudApp/CrudApp/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.SqlClient;
+using CrudApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CrudApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly string connectionString;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductDto productDto)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "INSERT INTO products " +
+                                 "(name, brand, category, price, description) VALUES " +
+                                 "(@name, @brand, @category, @price, @description)";
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", productDto.Name);
+                        command.Parameters.AddWithValue("@brand", productDto.Brand);
+                        command.Parameters.AddWithValue("@category", productDto.Category);
+                        command.Parameters.AddWithValue("@price", productDto.Price);
+                        command.Parameters.AddWithValue("@description", productDto.Description);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Product", "Sorry, but we have an exception.");
+                return BadRequest(ModelState);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ CrudApp/CrudApp/Models/ProductDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudApp.Models
+{
+    public class ProductDto
+    {
+        [Required]
+        public string Name { get; set; } = ""; 
+        [Required]
+        public string Brand { get; set; } = "";
+        [Required]
+        public string Category { get; set; } = "";
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Description { get; set; } = "";
+    }
+}
